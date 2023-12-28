d25bdb260f85358a7f2478929dbb39dce1f37b85
kijko <kijkomaciej@gmail.com>
2023-12-28 22:37:51 +0100

    backend initial commit

---
+++ backend-app/src/main/groovy/pl/edu/prz/baw/houston/fed/FedApplication.groovy
@@ -0,0 +1,13 @@
+package pl.edu.prz.baw.houston.fed
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FedApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FedApplication, args)
+	}
+
+}
---
+++ backend-app/src/test/groovy/pl/edu/prz/baw/houston/fed/FedApplicationTests.groovy
@@ -0,0 +1,13 @@
+package pl.edu.prz.baw.houston.fed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
