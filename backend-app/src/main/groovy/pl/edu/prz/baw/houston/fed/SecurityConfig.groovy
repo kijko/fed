13b0ee9e3a78547109e7f6206898f84cdd1ee749
kijko <kijkomaciej@gmail.com>
2023-12-31 14:16:15 +0100

    [backend] configure cors

--- backend-app/src/main/groovy/pl/edu/prz/baw/houston/fed/SecurityConfig.groovy
+++ backend-app/src/main/groovy/pl/edu/prz/baw/houston/fed/SecurityConfig.groovy
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
 import pl.edu.prz.baw.houston.fed.auth.JwtAuthFilter
 
 import static pl.edu.prz.baw.houston.fed.AppRouter.API_BASE_PATH
@@ -24,6 +25,17 @@ class SecurityConfig {
             .formLogin { it.disable()}
             .csrf { it.disable() }
             .logout { it.disable() }
+            .cors {
+                it.configurationSource {
+                    def cors = new CorsConfiguration()
+                    cors.setAllowedOrigins(List.of('http://localhost:3000', 'https://fed-app.azurewebsites.net'))
+                    cors.setAllowedMethods(List.of('*'))
+                    cors.setAllowedHeaders(List.of('*'))
+                    cors.setExposedHeaders(List.of('Authorization'))
+
+                    return cors
+                }
+            }
             .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("${API_BASE_PATH}/sign-in").permitAll()
                         .pathMatchers("${API_BASE_PATH}/users").hasRole('ADMIN')
